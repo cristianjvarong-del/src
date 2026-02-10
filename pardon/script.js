a0e8ba901c67f8d1584c3f1744904e50de99c7a7
cristianjvarong-del <cristianj.varong@gmail.com>
2026-02-10 17:06:24 -0500

    Add files via upload

---
+++ pardon/script.js
@@ -0,0 +1,13 @@
+const btnRandom = document.querySelector("#random-btn")
+
+function MoverAletoriaMente(btn) {  
+     btn.style.fontWeight = "bolder";
+     btn.style.position = "absolute";
+     btn.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+     btn.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+
+}
+
+btnRandom.addEventListener("mouseenter", function(e){
+   MoverAletoriaMente(e.target) 
+})
